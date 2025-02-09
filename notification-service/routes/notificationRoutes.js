20f7a72f21a433825487d776839194a9ee7879fb
Liew Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2025-02-09 19:22:08 +0800

    notification backend done and tested by postman

--- notification-service/routes/notificationRoutes.js
+++ notification-service/routes/notificationRoutes.js
@@ -4,9 +4,9 @@ const notificationController = require("../controllers/notificationController");
 const router = express.Router();
 
 router.get("/", notificationController.getAllNotifications);
-router.get("/:id", notificationController.getNotificationById);
+router.get("/:notificationId", notificationController.getNotificationById); // Use `notificationId`
 router.post("/", notificationController.createNotification);
-router.put("/:id", notificationController.updateNotification);
-router.delete("/:id", notificationController.deleteNotification);
+router.put("/:notificationId", notificationController.updateNotification); // Use `notificationId`
+router.delete("/:notificationId", notificationController.deleteNotification); // Use `notificationId`
 
 module.exports = router;
