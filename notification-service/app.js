fe33794d419883cb41d0b51ef23fa7e7f2ec23a9
Liew Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2025-02-09 23:31:00 +0800

    update

--- notification-service/app.js
+++ notification-service/app.js
@@ -13,7 +13,7 @@ app.use(express.json()); // Middleware to parse JSON body
 app.use("/notifications", notificationRoutes); // Notification routes
 app.use("/send-email", emailRoutes); // Email routes
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
